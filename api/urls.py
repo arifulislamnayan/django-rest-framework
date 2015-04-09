abee6cd437b014955bd576ac39c5f9b6b566b4b8
arifulislamnayan <farazi.aarif@gmail.com>
2015-04-09 15:27:41 +0600

    GET, POST, PUT, DELETE functionalty added

--- api/urls.py
+++ api/urls.py
@@ -8,7 +8,8 @@ from api import views
 urlpatterns = [
     
     url(r'^tasks/$', views.task_list, name='task_list'),
-    #url(r'^tasks/(?P<pk>[0-9]+)$', 'task_detail', name='task_detail'),
+    url(r'^tasks/(?P<pk>[0-9]+)$', views.task_detail, name='task_detail'),
+    
    
 
 ]
--- api/views.py
+++ api/views.py
@@ -26,5 +26,41 @@ def task_list(request):
 
 
 
-    		
+@api_view(['GET','PUT', 'DELETE'])
+def task_detail(request, pk):
+
+	try:
+		task= Task.objects.get(pk=pk)
+
+	except Task.DoesNotExist:
+		return Response(status= status.HTTP_404_NOT_FOUND)
+
+
+	if request.method=='GET':
+		serializer= TaskSerializer(task)
+		return Response(serializer.data)
+
+
+	elif request.method=='PUT':
+		serializer= TaskSerializer(task, data= request.DATA)
+		if serializer.is_valid():
+			serializer.save()
+			return Response(serializer.data)
+
+		else:
+			return Response(serializer.errors, status= status.HTTP_400_BAD_REQUEST)
+
+
+
+	elif request.method=='DELETE':
+		task.delete()
+		return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+		
+
+
+
+
+
 
